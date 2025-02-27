6494ec8233a1b0530b0c348b74afbbe4c61ba61a
Harish Suresh <harishsuresh@Harishs-MacBook-Pro.local>
2025-02-27 15:05:29 -0500

    homework3-part3 completed

--- app/src/main/java/com/example/lab7/TicketListAdapter.kt
+++ app/src/main/java/com/example/lab7/TicketListAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.lab7
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -17,6 +18,11 @@ class TicketHolder(
                 "${ticket.title} clicked!",
                 Toast.LENGTH_SHORT)
                 .show()
+        }
+        binding.ticketSolved.visibility = if (ticket.isSolved) {
+            View.VISIBLE
+        } else {
+            View.GONE
         }
     }
 }
