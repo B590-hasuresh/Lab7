ae808843e3e42b9287fc609e78683e921a91a0b5
Harish Suresh <harishsuresh@Harishs-MacBook-Pro.local>
2025-02-27 19:26:17 -0500

    bug fix

--- app/src/main/java/com/example/lab7/database/TicketDatabase.kt
+++ app/src/main/java/com/example/lab7/database/TicketDatabase.kt
@@ -7,8 +7,9 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
-import com.iub.lab7.database.TicketTypeConverter
-import com.iub.lab7.Ticket
+import com.example.lab7.Ticket
+import com.example.lab7.database.TicketTypeConverter
+
 
 @Database(entities = [Ticket::class], version = 1)
 @TypeConverters(TicketTypeConverter::class)
